b1b415ba68f5a57ce609273ac74db3c91e7681bb
Harsh Mahajan <harshmahajan2345@gmail.com>
2023-11-23 19:08:13 +0530

    hui

--- DevFusionProject/App.tsx
+++ DevFusionProject/App.tsx
@@ -3,22 +3,8 @@ import {Image, Text, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-
-function HomeScreen() {
-  return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Home!</Text>
-    </View>
-  );
-}
-
-function SettingsScreen() {
-  return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Settings!</Text>
-    </View>
-  );
-}
+import HomeScreen from './Screens/home_screen';
+import SettingsScreen from './Screens/startup_screen';
 
 const Tab = createBottomTabNavigator();
 
