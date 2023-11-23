b53e4f2da2895e435e24f0f4ae64ba513ed6c2d1
Harsh Mahajan <harshmahajan2345@gmail.com>
2023-11-23 18:55:09 +0530

    hi

--- DevFusionProject/App.tsx
+++ DevFusionProject/App.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
-import { Image, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {Image, Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
 function HomeScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text>Home!</Text>
     </View>
   );
@@ -14,7 +14,7 @@ function HomeScreen() {
 
 function SettingsScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text>Settings!</Text>
     </View>
   );
@@ -25,33 +25,57 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator screenOptions={({ route }) => ({
-          tabBarIcon: ( ({focused, color, size}) => {
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarIcon: ({focused, color, size}) => {
             let iconName;
 
             if (route.name === 'Home') {
-              if (! focused) {
-                return <Image source={require('./static/images/home-outline.png')} style={{height:30, width:30}}/>;
-              }
-              else {
-                return <Image source={require('./static/images/home.png')} style={{height:30, width:30}}/>;
+              if (!focused) {
+                return (
+                  <Image
+                    source={require('./static/images/home-outline.png')}
+                    style={{height: 30, width: 30}}
+                  />
+                );
+              } else {
+                return (
+                  <Image
+                    source={require('./static/images/home.png')}
+                    style={{height: 30, width: 30}}
+                  />
+                );
               }
             } else if (route.name === 'Maps') {
-              if (! focused) {
-                return <Image source={require('./static/images/home-outline.png')} style={{height:30, width:30}}/>;
+              if (!focused) {
+                return (
+                  <Image
+                    source={require('./static/images/home-outline.png')}
+                    style={{height: 30, width: 30}}
+                  />
+                );
+              } else {
+                return (
+                  <Image
+                    source={require('./static/images/home.png')}
+                    style={{height: 30, width: 30}}
+                  />
+                );
               }
-              else {
-                return <Image source={require('./static/images/home.png')} style={{height:30, width:30}}/>;
             }
-            }
-          }),
+          },
           tabBarActiveTintColor: '#000000',
           tabBarInactiveTintColor: 'gray',
           headerShown: false,
+          tabBarStyle: {
+            backgroundColor: '#ffffff',
+            height: 50,
+            paddingBottom: 5,
+          },
         })}>
-        <Tab.Screen name="Home" component={HomeScreen}/>
+        <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Maps" component={SettingsScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
-} 
+}
