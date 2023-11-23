6483eeceb5b9e0ec6c6bee7c24d834c54d51f49f
Harsh Mahajan <harshmahajan2345@gmail.com>
2023-11-23 19:12:19 +0530

    changes

--- DevFusionProject/Screens/home_screen.tsx
+++ DevFusionProject/Screens/home_screen.tsx
@@ -4,7 +4,7 @@ import {Text, View} from 'react-native';
 const Home = () => {
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Home Screen</Text>
+      <Text style={{color:"red"}}>Home Screen</Text>
     </View>
   );
 };
